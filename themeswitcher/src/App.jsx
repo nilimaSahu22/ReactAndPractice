0edde57484a15fec13f29c4f5525d5ea615db307
nilimaSahu22 <nilimasahu2201@gmail.com>
2025-05-01 17:01:42 +0530

    enabled dark mode

--- themeswitcher/src/App.jsx
+++ themeswitcher/src/App.jsx
@@ -5,7 +5,7 @@ import ThemeBtn from './components/ThemeBtn'
 import Card from './components/Card'
 
 function App() {
- const [themMode, setThemeMode] = useState("light")
+ const [themeMode, setThemeMode] = useState("light")
 
  const lightTheme = () => {
   setThemeMode("light")
@@ -17,12 +17,12 @@ function App() {
  // actual change in theme
  useEffect(() => {
   document.querySelector('html').classList.remove("light", "dark")
-  document.querySelector('html').classList.add(themMode)
- }, [themMode])
+  document.querySelector('html').classList.add(themeMode)
+ }, [themeMode])
 
   return (
     <ThemeProvider value={
-      {themMode, lightTheme, darkTheme}}>
+      {themeMode, lightTheme, darkTheme}}>
         <div className="flex flex-wrap min-h-screen items-center">
                         <div className="w-full">
                             <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
